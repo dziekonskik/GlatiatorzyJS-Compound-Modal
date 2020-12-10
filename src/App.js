0930d0ca5ba9fca62313813f49d59012042deba2
Seven <jaseveen@gmail.com>
2020-12-10 13:56:42 +0100

    moved opening modal to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
+import React, { Component } from 'react';
 import Modal from './Modal/Modal';
 import './App.css';
+class App extends Component {
+  state = {
+    isOpen: false,
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <Modal>
-        <Modal.Header title={'Tytuł modala'} />
-        <Modal.Content>
-          <h1>to powinno tak działać</h1>
-        </Modal.Content>
-        <Modal.Footer callToActionLabel={'Udało się'} />
-      </Modal>
-    </div>
-  );
+  toggle = () => {
+    this.setState((prevState) => {
+      return { isOpen: !prevState.isOpen };
+    });
+  };
+
+  render() {
+    const { isOpen } = this.state;
+    return (
+      <div className="App">
+        <button onClick={this.toggle}>Toggle Modal</button>
+        {isOpen && (
+          <Modal isOpen={this.state.isOpen} toggle={this.toggle}>
+            <Modal.Header title={'Tytuł modala'} />
+            <Modal.Content>
+              <h1>to powinno tak działać</h1>
+            </Modal.Content>
+            <Modal.Footer callToActionLabel={'Udało się'} />
+          </Modal>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
