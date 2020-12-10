59939d14e787eab756fecfd4a3d536534337c6de
Seven <jaseveen@gmail.com>
2020-12-10 14:42:44 +0100

    add Header ready

--- src/Modal/Header.js
+++ src/Modal/Header.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import ModalContext from '../modalContext';
 
 export class Header extends Component {
   render() {
-    return <div className="Header"></div>;
+    return (
+      <ModalContext.Consumer>
+        {(toggle) => (
+          <header className="Header">
+            <div>{this.props.title}</div>
+            <button onClick={toggle}>Close</button>
+          </header>
+        )}
+      </ModalContext.Consumer>
+    );
   }
 }
 
