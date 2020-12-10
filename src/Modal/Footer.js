412221a427dfaf61d44367cd87de2d1e367a0773
Seven <jaseveen@gmail.com>
2020-12-10 14:52:13 +0100

    add finished footer

--- src/Modal/Footer.js
+++ src/Modal/Footer.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import ModalContext from '../modalContext';
 
 export class Footer extends Component {
+  callToActionLabelFn = (fn) => {
+    window.alert('OK');
+    fn();
+  };
+
   render() {
-    return <div className="Footer"></div>;
+    return (
+      <ModalContext.Consumer>
+        {(toggle) => (
+          <div className="Footer">
+            <button onClick={toggle}>Cancel</button>
+            <button onClick={() => this.callToActionLabelFn(toggle)}>
+              {this.props.callToActionLabel}
+            </button>
+          </div>
+        )}
+      </ModalContext.Consumer>
+    );
   }
 }
 
