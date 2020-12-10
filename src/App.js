cc65c4cae5e3b852beeb1c73283b347ead5b81c2
Seven <jaseveen@gmail.com>
2020-12-10 11:58:41 +0100

    add file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import Modal from './Modal/Modal';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Modal />
+      return (
+      <Modal>
+        <Modal.Header title={'Tytuł modala'} />
+        <Modal.Content>
+          <h1>to powinno tak działać</h1>
+        </Modal.Content>
+        <Modal.Footer callToActionLabel={'Udało się'} />
+      </Modal>
+      );
     </div>
   );
 }
--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 export class Modal extends Component {
   render() {
-    return (
-      <Modal>
-        <Modal.Header title={'Tytuł modala'} />
-        <Modal.Content>
-          <h1>to powinno tak działać</h1>
-        </Modal.Content>
-        <Modal.Footer callToActionLabel={'Udało się'} />
-      </Modal>
+    const children = React.Children.map(this.props.children, (child) =>
+      console.log(this.props.children, child)
     );
+    return <div>{children}</div>;
   }
 }
 
